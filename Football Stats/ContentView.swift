13f3e2469c83e4cf7c68b082225bacd93ea20a38
Kelvin Campos Ricafort <kelvs.r@outlook.com>
2026-09-05 15:44:07 +0800

    Initial Commit

---
+++ Football Stats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football Stats
+//
+//  Created by Kelvin Ricafort on 9/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football Stats/Football_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_StatsApp.swift
+//  Football Stats
+//
+//  Created by Kelvin Ricafort on 9/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Football_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
